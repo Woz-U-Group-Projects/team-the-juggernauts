d616c71a916180ff516308759559d742d781fa5e
mechanicalbob88 <mechanicalbob88@yahoo.com>
2019-05-21 18:13:37 -0400

    removed "views" not needed for React w/ Express. plus added models and routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../models/users')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/person/:id', function(req, res, next) {
+  //get object that matches the id
+  let person = users.people.find(peep => {
+    return peep.id === parseInt(req.params.id);
+  });
+  // render the template with that object
+  res.render('index', { person });
+  console.log(req.url);
 });
+
+
 
 module.exports = router;
