043ad7b6bdf65662701912d5a139cbdb7b607736
mechanicalbob88 <mechanicalbob88@yahoo.com>
2019-05-24 21:37:23 -0400

    Update Home.js

--- react/src/screens/Home.js
+++ react/src/screens/Home.js
@@ -8,7 +8,7 @@ class MyForm extends React.Component {
     render() {
       return (
         <div>
-        <Header title="Home" />
+        <Header title="Welcome!" />
         <form>
           <h1>Hello</h1>
           <p>Enter your name:</p>
