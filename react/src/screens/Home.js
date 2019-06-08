0676376366074ff7fd65658b3496445b4c97dae6
mechanicalbob88 <mechanicalbob88@yahoo.com>
2019-06-07 20:29:46 -0400

    Update Home.js

--- react/src/screens/Home.js
+++ react/src/screens/Home.js
@@ -35,69 +35,69 @@ class SignUpForm extends React.Component {
     return (
 
       <body>
-      <div>
-      <Header title="Welcome to"></Header>
-
-
-     
-  <form>
-        <container id= " welcome">Pet Play</container>
-<img id="tugawar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHsdvsfKB43bNPiPEpnHEGEO0RV18mt51VU1ZNk-A21TStPjVF0g" alt="tuawar"></img>
+        <div>
+          <Header title="Welcome to"></Header>
 
 
 
-        <p id="signup">Sign-Up:</p>
-        <input
-          value={this.state.newUser}
-          onChange={evt => this.setState({ newUser: evt.target.value })}
-        />
-      </form>
-
-
-      <div id= "pic"> 
-
-
-      <div id="banner">
-  <div id="cloud-scroll">
+          <form>
+            <container id=" welcome">Pet Play</container>
+            <img id="tugawar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHsdvsfKB43bNPiPEpnHEGEO0RV18mt51VU1ZNk-A21TStPjVF0g" alt="tuawar"></img>
 
 
 
-
-      <p id="pone">A wonderful place of fun and play for your pets</p>
-
-      <div id="imgone" alt="imgone">
-      <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6pmwTBzF6u54Uua0b1ZHc2lOjLpTRM7JUrYizrsTX7csvm4Mwew" class="responsive" alt="cute"></img>
-      </div>
-
-      <div id="imgtwo">
-      <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQRKLa8Pn1PgHlww1gZq-5p-5tL1455-KFiHVRE7Ka0ZIp-kOe1w" class="responsive" alt="beach"></img>
-      </div>
+            <p id="signup">Sign-Up:</p>
+            <input
+              value={this.state.newUser}
+              onChange={evt => this.setState({ newUser: evt.target.value })}
+            />
+          </form>
 
 
-      <p id ="ptwo">A Great way to explore your community find friends and neighbors with similiar intrests and likes</p>
+          <div id="pic">
 
 
-
-      <div id="imgthree">
-      <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVFzY2w3EFkUDJkmZQXO6tuNnSr-nmsV4W5nSUZGxTm7GsT66H" class="responsive"alt="friends" ></img>
-      </div>
-
-      <div id="imgfour">
-      <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK2aeoPDxz8egsbLh9kYADsGe8dq79B_zFvO1P38nNUiY6a1KAUg" class="responsive"alt="heeey" ></img>
-      </div>
-      
-      
-      
-      
-      
-      </div>
-  </div>
-</div>
+            <div id="banner">
+              <div id="cloud-scroll">
 
 
 
-    
-      </div>
+
+                <p id="pone">A wonderful place of fun and play for your pets</p>
+
+                <div id="imgone" alt="imgone">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6pmwTBzF6u54Uua0b1ZHc2lOjLpTRM7JUrYizrsTX7csvm4Mwew" class="responsive" alt="cute"></img>
+                </div>
+
+                <div id="imgtwo">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQRKLa8Pn1PgHlww1gZq-5p-5tL1455-KFiHVRE7Ka0ZIp-kOe1w" class="responsive" alt="beach"></img>
+                </div>
+
+
+                <p id="ptwo">A Great way to explore your community find friends and neighbors with similiar intrests and likes</p>
+
+
+
+                <div id="imgthree">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVFzY2w3EFkUDJkmZQXO6tuNnSr-nmsV4W5nSUZGxTm7GsT66H" class="responsive" alt="friends" ></img>
+                </div>
+
+                <div id="imgfour">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK2aeoPDxz8egsbLh9kYADsGe8dq79B_zFvO1P38nNUiY6a1KAUg" class="responsive" alt="heeey" ></img>
+                </div>
+
+
+
+
+
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </div>
 
       </body>
     );
