38d6b5361e21ac5bfa6fcb910f8c3768f7f8eb04
Ryan Berdel <nanokarbon@outlook.com>
2019-06-13 19:34:23 -0400

    Created Initial Post Model

--- models/post.js
+++ models/post.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define('Post', {
-    name: DataTypes.STRING
+    post_id: DataTypes.INTEGER.UNSIGNED,
+    pet_id: DataTypes.INTEGER.UNSIGNED,
+    post_body: DataTypes.STRING,
+    img_url: DataTypes.STRING,
+    link_url: DataTypes.STRING
   }, {});
   Post.associate = function(models) {
     // associations can be defined here
