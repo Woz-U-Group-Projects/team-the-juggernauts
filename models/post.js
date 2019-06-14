64e18432cccb1bf9a1b75e2deffb8b59ed6b3e24
Ryan Berdel <nanokarbon@outlook.com>
2019-06-13 21:08:26 -0400

    Revert "Created Initial Post Model"
    This reverts commit 38d6b5361e21ac5bfa6fcb910f8c3768f7f8eb04.

--- models/post.js
+++ models/post.js
@@ -1,11 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define('Post', {
-    post_id: DataTypes.INTEGER.UNSIGNED,
-    pet_id: DataTypes.INTEGER.UNSIGNED,
-    post_body: DataTypes.STRING,
-    img_url: DataTypes.STRING,
-    link_url: DataTypes.STRING
+    name: DataTypes.STRING
   }, {});
   Post.associate = function(models) {
     // associations can be defined here
