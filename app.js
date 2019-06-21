47da65b6513478d7f04e35e7c7113acb4ea798db
mechanicalbob88 <mechanicalbob88@yahoo.com>
2019-06-21 17:18:03 -0400

    post

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var cors = require ('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var postsRouter = require('./routes/post');
 
 var app = express();
 app.use(cors());
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/post', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- client/src/screens/Me.js
+++ client/src/screens/Me.js
@@ -1,25 +1,110 @@
 import React from 'react';
 import Header from '../components/Header';
-
+import axios from "axios";
+import {withRouter} from 'react-router-dom';
 
 
 class Post extends React.Component {
-    
-    getUser() {
-        
+    state = {
+        post: [],
+        newPost: "",
+        searchPost: "",
+        submitPost: ""
+    };
+
+    getPost() {
+        axios.get("http://localhost:5000/fruit").then(res => {
+            const post = res.data;
+            this.setState({ post });
+        });
     }
-    
-    
-    
-    
-    
-    
+
+    componentDidMount() {
+        this.getPost();
+    }
+
+    submitPost() {
+        axios
+            .post("http://localhost:3001/posts", { post_body: this.state.newPost })
+            .then(res => {
+                this.setState({ newPost: "" }); //Reset new fruit field to blank.
+                this.getPost();
+                this.props.history.push("/posts");
+            });
+    }
+
+    searchPost(searchTerm) {
+        if (searchTerm === "") {
+            this.setState({ searchPost: "" }); //Clearing search fruit eliminates odd behavior when clearing search field.
+            this.getPost();
+        } else {
+            this.setState({ searchPost: searchTerm }); //This is necessary or text input will hold only one character.
+            axios.get("http://localhost:3001/posts/search/" + searchTerm)
+                .then(res => {
+                    const post = res.data;
+                    this.setState({ post });
+                });
+        }
+    }
+
+    deletePost(id) {
+        axios.delete("http://localhost:3001/posts/" + id + "")
+            .then(res => {
+                console.log(res.body);
+                this.getPost();
+            });
+    }
+
+    editPost(id, name) {
+        let result = prompt('Edit Post Name', name);
+        if (result != null) name = result;
+        axios
+            .post("http://localhost:3001/posts/edit",
+                { "id": id, "name": name })
+            .then(res => {
+                console.log(res);
+                this.getPost(); //Refresh
+            });
+    }
     render() {
         return (
-            <div><Header title="Me" /></div>
-        )
- 
+            <div>
+                <Header title="Connect with friends!" />
+                <h3>Search</h3>
+                <input
+                    value={this.state.searchPost}
+                    onChange={evt => this.searchPost(evt.target.value)}
+                />
+                <button onClick={() => this.searchPost("")}>Reset</button>
+
+                <h1>Post:</h1>
+                <ul>
+                    {this.state.post.map(f => (
+                        <li key={f.id}>
+                            <button onClick={() => this.deletePost(f.id)}>X</button>
+                            <button onClick={() => this.editPost(f.id, f.name)}>Edit</button>
+                            <nbsp /> {f.name}
+                            <ul>
+                                <li>{f.description}</li>
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+
+                <h3>Add Post</h3>
+                <input
+                    value={this.state.newPost}
+                    onChange={evt => this.setState({ newPost: evt.target.value })}
+                />
+                <button onClick={() => this.submitPost()}>Submit</button>
+            </div>
+        );
+
     }
 }
 
-export default Post;
+export default withRouter( Post);
+
+// Fruit to Post
+// newFruit to newPost
+// searchFruits searchPost
