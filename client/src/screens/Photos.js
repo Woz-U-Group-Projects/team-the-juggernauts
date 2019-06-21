390f5035af17bf21de566651f7de05ed77a11420
mechanicalbob88 <mechanicalbob88@yahoo.com>
2019-06-20 20:55:09 -0400

    attempt

--- client/src/screens/Photos.js
+++ client/src/screens/Photos.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import Header from '../components/Header';
+import Spinner from './Spinner';
+import Buttons from './Buttons'
+// import Images from './Images'
+// import Images from './Images';
 
-const Photos = () => <Header title="Photos" />;
+// const Photos = () => <Header title="Photos" />;
 
-export default Photos;
+class Image extends React.Component {
+
+render() {
+    return(
+        <div>
+            <Header title="Photos" />;
+            <Spinner/>
+            <Buttons/>
+            {/* <Images/> */}
+        </div> 
+    );
+}
+} 
+
+
+
+export default Image;
